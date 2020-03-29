136cfeb72299512c4fce7c5cb971f8d1c6e50939
Ben Green <ben@bcgdesign.com>
2020-03-29 13:05:15 +0100

    Adding README.md for Powershell folder Improving readability of cleanup-folder.ps1

--- powershell/README.md
+++ powershell/README.md
@@ -5,3 +5,5 @@
 Removes all files from a specified folder that are older than the set number of days.
 
 Useful for backups / log files.
+
+`curl https://raw.githubusercontent.com/bencgreen/scripts/master/powershell/cleanup-folder.ps1 > cleanup-folder.ps1`
--- powershell/cleanup-folder.ps1
+++ powershell/cleanup-folder.ps1
@@ -1,3 +1,5 @@
 Set-Variable -Name "path" -Value ""
 Set-Variable -Name "days" -Value 28
-Get-ChildItem -path "$path\*.*" |? {$_.LastWriteTime -lt (Get-Date).AddDays(-$days)} | del
+Get-ChildItem -path "$path\*.*" `
+	| Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-$days)} `
+	| Remove-Item
