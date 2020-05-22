5bbc26d3a299dfc4a9b8bcfb04186a826857cbdd
Ben Green <ben@bcgdesign.com>
2020-05-22 16:07:28 +0100

    Removing unnecessary cho

--- batch/add-sources.bat
+++ batch/add-sources.bat
@@ -1,4 +1,2 @@
 for %%a in ("%cd%") do set "CurDir=%%~na"
-echo For example: %CurDir%
-
-MKLINK /D "Sources" "F:\OneDrive - bcg design\Online Services\%CurDir%"
+MKLINK /D "Sources" "D:\OneDrive - bcg design\Online Services\%CurDir%"
