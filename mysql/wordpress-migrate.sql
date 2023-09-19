34e3c43cf38764af542c5dddd0281d9c6e514488
bfren <bf@bfren.dev>
2023-09-19 14:31:43 +0100

    Removing URLs from script

--- mysql/wordpress-migrate.sql
+++ mysql/wordpress-migrate.sql
@@ -1,9 +1,8 @@
-SET @o = "https://bcg.xyz/portfolio";
-SET @n = "https://rp.bcg.xyz/portfolio";
+SET @o = ""; -- URL to search for
+SET @n = ""; -- URL to replace with
 
 UPDATE wp_posts p
-SET p.post_content = REPLACE(p.post_content, @o, @n)
-, p.guid = REPLACE(p.guid, @o, @n);
+SET p.post_content = REPLACE(p.post_content, @o, @n);
 
 UPDATE wp_postmeta pm
 SET pm.meta_value = REPLACE(pm.meta_value, @o, @n);
